60b43dc414a78825f39ca5b06e3120d26b9a781a
mia <miabenn74@gmail.com>
2023-10-05 08:20:08 +1000

    Initial Commit

---
+++ SwiftDataToDo/SwiftDataToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataToDoApp.swift
+//  SwiftDataToDo
+//
+//  Created by Mia on 5/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
